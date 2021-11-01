0ee5d471af6c250e15f5d9c97fddc28acf5894e7
Sasha-Tolk <tolkachiowa@gmail.com>
2021-11-01 23:58:49 +0200

    first commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems}  from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryDiv = document.querySelector(".gallery");
+const galleryImg = document.querySelector(".gallery__image");
 
-function createCard() {
-    return galleryItems.map((preview, original, description) => {
-        return '<img class = "gallery__image">  src = `${preview}` alt = `${description}` </img>'
-    }).join("");  
-}
+function createCard (galleryItems) {   
+return galleryItems.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src=${preview}
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    }).join('');
+};
 
 const cards = createCard(galleryItems);
-galleryDiv.append(cards);
+galleryDiv.insertAdjacentHTML('beforeend', cards);
+
+galleryDiv.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+galleryDiv.addEventListener('click', openModal);
+
+function openModal(event){
+    const isModalOpenImg = event.target.classList.contains('gallery__image');
+    
+    if(isModalOpenImg){
+        const bigImg = event.target.getAttribute("data-source");
+        galleryImg.src = bigImg
+
+
+        console.log(galleryImg)
+    }
+}
